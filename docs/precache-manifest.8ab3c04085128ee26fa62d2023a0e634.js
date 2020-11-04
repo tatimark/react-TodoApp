2c0790a1452718140eabce8f273298b8457ea12d
Tania Roman <pbtaniaroman@gmail.com>
2020-11-04 20:45:31 +0100

    add docs

---
+++ docs/precache-manifest.8ab3c04085128ee26fa62d2023a0e634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195bbc832b3165d2fa20a92863be27e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427c02488965a7bbcafa",
+    "url": "/static/css/main.96e11195.chunk.css"
+  },
+  {
+    "revision": "bd715bb67e2f4d2b970c",
+    "url": "/static/js/2.ceafc49f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ceafc49f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427c02488965a7bbcafa",
+    "url": "/static/js/main.a510255a.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
